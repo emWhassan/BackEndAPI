342658a3ee2e8da8a90f80de82446ca25722bee4
enWhassan <ch.waseem66@gmail.com>
2021-12-15 21:42:15 +0100

    add migration

---
+++ GitCoreWebAPI/Data/DataContext/ManagementCatalog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GitCoreWebAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GitCoreWebAPI.Data.DataContext
+{
+    public class ManagementCatalogDBContext:DbContext
+    {
+        public ManagementCatalogDBContext(DbContextOptions<ManagementCatalogDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
--- GitCoreWebAPI/Startup.cs
+++ GitCoreWebAPI/Startup.cs
@@ -1,6 +1,8 @@
+using GitCoreWebAPI.Data.DataContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace GitCoreWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ManagementCatalogDBContext>(m => m.UseSqlServer(Configuration["ConnectionStrings:NTPWebShopDBConnectionString"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
